0e6e377f9be7a66de9ffc99453a8201409800de2
SuniDev <sun-918@naver.com>
2024-04-24 19:58:42 +0900

    Creating a watchOS app
    - https://developer.apple.com/tutorials/swiftui/creating-a-watchos-app

---
+++ Landmarks/WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by suni on 4/24/24.
+//
+
+import SwiftUI
+import UserNotifications
+
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
+    }
+}
+
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
